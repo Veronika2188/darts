d1ba4a0be49b4b47eb2f8c0a87455998bbefc089
darvironia <veronika.gazdovskaya@gmail.com>
2024-02-12 20:45:36 +0300

    Start new dart project

---
+++ bin/darts.dart
@@ -0,0 +1,5 @@
+import 'package:darts/darts.dart' as darts;
+
+void main(List<String> arguments) {
+  print('Hello world: ${darts.calculate()}!');
+}
---
+++ test/darts_test.dart
@@ -0,0 +1,8 @@
+import 'package:darts/darts.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
